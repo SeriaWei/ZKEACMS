69155d48100ddcee8324f5e90916781279acd1e4
Wayne <seriawei@gmail.com>
2017-04-18 12:39:04 +0800

    .

--- Publish.cmd
+++ Publish.cmd
@@ -5,9 +5,16 @@
 @echo 该工具将帮助您发布您的 ZKEACMS 程序
 @echo 在开始之前请您先查看我们的许可协议
 @echo http://www.zkea.net/licenses
+@echo -----------------------------------------------------------------------------
+@echo 有关于.Net Core应用程序的安装部署，请查看 Microsoft Docs
+@echo .NET Core 应用程序部署
+@echo https://docs.microsoft.com/zh-cn/dotnet/articles/core/deploying
+@echo .NET Core 运行时标识符 (RID) 目录
+@echo https://docs.microsoft.com/zh-cn/dotnet/articles/core/rid-catalog
+@echo -----------------------------------------------------------------------------
 @pause
 cd src/ZKEACMS.WebHost
-dotnet publish -o ./bin/Release/PublishOutput
+dotnet publish -c Release -o ./bin/Release/PublishOutput
 cd ../../
 cd PluginPublisher
 dotnet restore
