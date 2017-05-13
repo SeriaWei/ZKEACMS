d8db6b85c765f407b54e00cc8c6ab05a60a342c5
Wayne <seriawei@outlook.com>
2017-05-13 14:36:21 +0800

    product properties

--- src/ZKEACMS/Product/Models/Product.cs
+++ src/ZKEACMS/Product/Models/Product.cs
@@ -31,6 +31,8 @@ namespace ZKEACMS.Product.Models
         /// 类别
         /// </summary>
         public int? ProductCategoryID { get; set; }
+
+        public string PartNumber { get; set; }
         /// <summary>
         /// 颜色
         /// </summary>
@@ -70,7 +72,6 @@ namespace ZKEACMS.Product.Models
         public DateTime? PublishDate { get; set; }
         public string TargetFrom { get; set; }
         public string TargetUrl { get; set; }
-        
         public IEnumerable<ExtendFieldEntity> ExtendFields { get; set; }
 
     }
@@ -86,10 +87,6 @@ namespace ZKEACMS.Product.Models
             ViewConfig(m => m.BrandCD).AsHidden();
             ViewConfig(m => m.ProductCategoryID).AsDropDownList().Required().DataSource(ViewDataKeys.ProductCategory, SourceType.ViewData).AddClass("select").AddProperty("data-url", "/admin/ProductCategory/Select");
             ViewConfig(m => m.ExtendFields).AsListEditor();
-            ViewConfig(m => m.ShelfLife).AsHidden();
-            ViewConfig(m => m.Norm).AsHidden();
-            ViewConfig(m => m.Color).AsHidden();
-            ViewConfig(m => m.PurchasePrice).AsHidden();
             ViewConfig(m => m.ProductContent).AsTextArea().AddClass("html");
             ViewConfig(m => m.Description).AsTextArea();
             ViewConfig(m => m.IsPublish).AsTextBox().Hide().ShowInGrid();
