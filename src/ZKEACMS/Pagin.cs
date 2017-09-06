318a659376c66a1c7ffd6e87f6b88b0919676a63
Wayne <seriawei@outlook.com>
2017-09-06 15:41:41 +0800

    regularPagination

---
+++ src/ZKEACMS/Pagin.cs
@@ -0,0 +1,17 @@
+﻿/* http://www.zkea.net/ 
+ * Copyright 2017 
+ * ZKEASOFT 
+ * http://www.zkea.net/licenses 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZKEACMS
+{
+    public class Pagin : Easy.RepositoryPattern.Pagination
+    {
+        public string BaseUrlFormat { get; set; }
+    }
+}
