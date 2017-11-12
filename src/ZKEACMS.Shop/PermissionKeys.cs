5e7f5965ca8a101d1655c7430356e0d8441f2e37
ZKEASOFT <411367956@qq.com>
2017-11-12 23:09:20 +0800

    ZKEACMS.Shop

---
+++ src/ZKEACMS.Shop/PermissionKeys.cs
@@ -0,0 +1,13 @@
+/* http://www.zkea.net/ Copyright 2017
+ * ZKEASOFT 
+ * http://www.zkea.net/licenses 
+ */
+
+namespace ZKEACMS.Shop
+{
+    public class PermissionKeys
+    {
+        public const string ViewOrder = "Order_View";
+        public const string ManageOrder = "Order_Manage";
+    }
+}
---
+++ src/ZKEACMS.Shop/ProductDbContext.cs
@@ -0,0 +1,22 @@
+﻿/* http://www.zkea.net/ Copyright 2017
+ * ZKEASOFT 
+ * http://www.zkea.net/licenses 
+ */
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZKEACMS.Product.Models;
+using Easy.RepositoryPattern;
+
+namespace ZKEACMS.Shop
+{
+    public class OrderDbContext : CMSDbContext
+    {
+        public OrderDbContext(IEnumerable<IOnModelCreating> modelCreatings, IOnDatabaseConfiguring configuring) : base(modelCreatings, configuring)
+        {
+        }
+    }
+}
