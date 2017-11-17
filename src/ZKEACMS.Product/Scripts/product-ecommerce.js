9afc21304247e3970f91bef5799d9b2c8d511e6e
ZKEASOFT <411367956@qq.com>
2017-11-17 23:43:07 +0800

    ecommerce

--- src/ZKEACMS.Product/Scripts/product-ecommerce.js
+++ src/ZKEACMS.Product/Scripts/product-ecommerce.js
@@ -7,4 +7,18 @@
         $(".stage", ecommerce).attr("src", $(this).data("src"));
         $(this).parent().addClass("active").siblings().removeClass("active");
     });
+    ecommerce.on("click", ".quantity-minus", function () {
+        var quantity = Number($.trim($(this).closest(".quantity-set").find(".quantity").text()));
+        if (quantity > 1) {
+            quantity -= 1;
+            ecommerce.find(".add-to-basket").data("quantity", quantity);
+            $(this).closest(".quantity-set").find(".quantity").text(quantity);
+        }
+    });
+    ecommerce.on("click", ".quantity-plus", function () {
+        var quantity = Number($.trim($(this).closest(".quantity-set").find(".quantity").text()));
+        quantity += 1;
+        ecommerce.find(".add-to-basket").data("quantity", quantity);
+        $(this).closest(".quantity-set").find(".quantity").text(quantity);
+    });
 });
