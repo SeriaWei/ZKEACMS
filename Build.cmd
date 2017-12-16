a1f470dda6719215b1769554b8ab0d55113d2344
ZKEASOFT <411367956@qq.com>
2017-12-16 22:35:44 +0800

    。

--- Build.cmd
+++ Build.cmd
@@ -1,2 +1,3 @@
 @echo off
+@echo Building...
 dotnet build ZKEACMS.sln
--- Run.cmd
+++ Run.cmd
@@ -1,5 +1,10 @@
 @echo off
 echo ASPNETCORE_ENVIRONMENT=Development
 set ASPNETCORE_ENVIRONMENT=Development
+
+if not exist "src/ZKEACMS.WebHost/bin/Debug/netcoreapp2.0/ZKEACMS.WebHost.dll" (
+call Build.cmd
+)
+
 cd src/ZKEACMS.WebHost
 dotnet bin/Debug/netcoreapp2.0/ZKEACMS.WebHost.dll
