f17e05742fe25ec26ad98248aff13a73e03aac83
Wayne <seriawei@outlook.com>
2018-05-03 10:12:14 +0800

    Update Description nvarchar(500)

--- DataBase/Update/2.9/Scriptsql.sql
+++ DataBase/Update/2.9/Scriptsql.sql
@@ -1,4 +1,5 @@
-ALTER TABLE Navigation ADD Html NVARCHAR(max)
+ALTER TABLE dbo.Navigation ADD Html NVARCHAR(max)
+ALTER TABLE dbo.Navigation ALTER COLUMN Description NVARCHAR(500)
 GO
 INSERT [dbo].[Language] ([LanKey], [CultureName], [LanValue], [Module], [LanType]) VALUES (N'NavigationEntity@Html', N'zh-CN', N'HTMLÄÚÈÝ', N'NavigationEntity', N'EntityProperty')
 GO
