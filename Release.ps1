568632995fd60d4cf9c92d7877714660b2894176
Wayne <seriawei@gmail.com>
2018-06-03 18:01:52 +0800

    install dotnet normal

--- Release.ps1
+++ Release.ps1
@@ -3,11 +3,6 @@ $source = "Release"
 $destination = "ZKEACMS.Core.v3.0.zip"
 Write-Host "Starting release" $destination
 Write-Host "This may take a few minutes, please wait..."
-if(!(Get-Command dotnet -ErrorAction SilentlyContinue))
-{
-    Write-Host "Installing .Net Core SDK..."
-   ./dotnet-install.ps1 -Channel Current -Version latest
-}
 if(Test-Path $source){
     Remove-Item -Path $source -Force -Recurse
 }
