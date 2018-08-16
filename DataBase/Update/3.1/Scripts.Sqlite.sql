225b8a9500822bcd087e37d147df4365c38d78bf
Wayne <seriawei@outlook.com>
2018-08-16 17:25:11 +0800

    UTF-8

--- DataBase/Update/3.1/Scripts.Sqlite.sql
+++ DataBase/Update/3.1/Scripts.Sqlite.sql
@@ -3,7 +3,7 @@ ALTER TABLE ArticleType ADD Url NVARCHAR(100);
 ALTER TABLE ProductCategory ADD Url NVARCHAR(100);
 ALTER TABLE Forms ADD NotificationReceiver NVARCHAR(500);
 
-INSERT INTO [Language] ([LanKey], [CultureName], [LanValue], [Module], [LanType]) VALUES ('Robots@Content', 'zh-CN', '内容', 'Robots', 'EntityProperty');
-INSERT INTO [Language] ([LanKey], [CultureName], [LanValue], [Module], [LanType]) VALUES ('MessageNotificationConfig@MessageNotifyEmails', 'zh-CN', '新留言通知邮箱', 'MessageNotificationConfig', 'EntityProperty');
-INSERT INTO [Language] ([LanKey], [CultureName], [LanValue], [Module], [LanType]) VALUES ('MessageNotificationConfig@CommentNotifyEmails', 'zh-CN', '新评论通知邮箱', 'MessageNotificationConfig', 'EntityProperty');
+INSERT INTO [Language] ([LanKey], [CultureName], [LanValue], [Module], [LanType]) VALUES ('Robots@Content', 'zh-CN', '鍐呭', 'Robots', 'EntityProperty');
+INSERT INTO [Language] ([LanKey], [CultureName], [LanValue], [Module], [LanType]) VALUES ('MessageNotificationConfig@MessageNotifyEmails', 'zh-CN', '鏂扮暀瑷�閫氱煡閭', 'MessageNotificationConfig', 'EntityProperty');
+INSERT INTO [Language] ([LanKey], [CultureName], [LanValue], [Module], [LanType]) VALUES ('MessageNotificationConfig@CommentNotifyEmails', 'zh-CN', '鏂拌瘎璁洪�氱煡閭', 'MessageNotificationConfig', 'EntityProperty');
 
