1af6f5c59305341ece808756482bf7610be1fb2b
Wayne <seriawei@outlook.com>
2018-08-20 13:43:17 +0800

    improve and perfect the interface

--- src/EasyFrameWork/Cache/ICacheManager.cs
+++ src/EasyFrameWork/Cache/ICacheManager.cs
@@ -7,7 +7,13 @@ using System;
 namespace Easy.Cache
 {
     public interface ICacheManager<T>
-    {
+    {   
+        bool Add(string key, T value);
+        bool Add(string key, T value, string region);
+        bool Exists(string key);
+        bool Exists(string key, string region);
+        T Get(string key);
+        T Get(string key, string region);
         T GetOrAdd(string key, string region, Func<string, string, T> valueFactory);
         T GetOrAdd(string key, Func<string, T> valueFactory);
         T GetOrAdd(string key, string region, T value);
