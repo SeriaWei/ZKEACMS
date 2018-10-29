1cc0da225770d91100f5f764f6ade2b4b7ef2e2d
Wayne <seriawei@outlook.com>
2018-10-29 17:22:00 +0800

    Site search

--- src/ZKEACMS.SiteSearch/Models/SiteSearchWidget.cs
+++ src/ZKEACMS.SiteSearch/Models/SiteSearchWidget.cs
@@ -1,4 +1,5 @@
-﻿using Easy.MetaData;
+﻿using Easy;
+using Easy.MetaData;
 using Easy.Models;
 using System;
 using System.Collections.Generic;
@@ -13,13 +14,26 @@ namespace ZKEACMS.SiteSearch.Models
 {
     public class SiteSearchWidget : SimpleWidgetBase
     {
-        
+        public string SearchEngine { get; set; }
     }
     class SiteSearchWidgetMetaData : WidgetMetaData<SiteSearchWidget>
     {
         protected override void ViewConfigure()
         {
             base.ViewConfigure();
+            ViewConfig(m => m.SearchEngine).AsDropDownList().DataSource(() =>
+            {
+                var searchEngines = ServiceLocator.GetService<IEnumerable<SearchEngine>>();
+                Dictionary<string, string> dics = new Dictionary<string, string>();
+                foreach (var item in searchEngines)
+                {
+                    if (!dics.ContainsKey(item.SearchQuery))
+                    {
+                        dics.Add(item.SearchQuery, item.Name);
+                    }
+                }
+                return dics;
+            });
         }
     }
 }
