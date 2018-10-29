da85fc794a50dfc30034f201082e0e5039c089c7
Wayne <seriawei@outlook.com>
2018-10-29 15:59:32 +0800

    SiteSearch

---
+++ src/ZKEACMS.SiteSearch/Models/SiteSearchWidget.cs
@@ -0,0 +1,25 @@
+﻿using Easy.MetaData;
+using Easy.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using ZKEACMS.MetaData;
+using ZKEACMS.Widget;
+
+namespace ZKEACMS.SiteSearch.Models
+{
+    public class SiteSearchWidget : SimpleWidgetBase
+    {
+        
+    }
+    class SiteSearchWidgetMetaData : WidgetMetaData<SiteSearchWidget>
+    {
+        protected override void ViewConfigure()
+        {
+            base.ViewConfigure();
+        }
+    }
+}
