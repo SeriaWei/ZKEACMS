6eed5c1caa65978237079b8186836f75f6d7b70f
Wayne <seriawei@gmail.com>
2018-12-05 23:54:50 +0800

    Runtime-2.2

--- src/EasyFrameWork/Mvc/Plugin/Loader.cs
+++ src/EasyFrameWork/Mvc/Plugin/Loader.cs
@@ -19,9 +19,9 @@ namespace Easy.Mvc.Plugin
         public const string PluginFolder = "Plugins";
         private const string PluginInfoFile = "zkea.plugin";
 #if DEBUG
-        private string[] AltDevelopmentPath = new[] { "bin", "Debug", "netcoreapp2.1" };
+        private string[] AltDevelopmentPath = new[] { "bin", "Debug", "netcoreapp2.2" };
 #else
-        private string[] AltDevelopmentPath = new[] { "bin", "Release", "netcoreapp2.1" };
+        private string[] AltDevelopmentPath = new[] { "bin", "Release", "netcoreapp2.2" };
 #endif
         private static List<AssemblyLoader> Loaders = new List<AssemblyLoader>();
         private static Dictionary<string, Assembly> LoadedAssemblies = new Dictionary<string, Assembly>();
