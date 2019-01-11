74c889a8faa56301ce69c883e6747c2a6799fb03
Wayne <seriawei@outlook.com>
2019-01-11 17:02:52 +0800

    LocalStorage

--- src/EasyFrameWork/Storage/IStorageService.cs
+++ src/EasyFrameWork/Storage/IStorageService.cs
@@ -12,5 +12,6 @@ namespace Easy.Storage
         string SaveFile(Stream stream, string directory, string fileName);
         void AppendFile(Stream stream, string filePath);
         void Delete(string filePath);
+        void DeleteDirectory(string path);
     }
 }
---
+++ src/EasyFrameWork/Storage/LocalStorage.cs
@@ -0,0 +1,52 @@
+﻿using Easy.Extend;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Easy.Storage
+{
+    public class LocalStorage : IStorageService
+    {
+        protected virtual string MapPath(string dictionary)
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), dictionary.TrimStart('~').ToFilePath());
+        }
+        protected virtual string GenerateDictionary(string fileName)
+        {
+            return Path.Combine("Files", fileName);
+        }
+        public virtual void AppendFile(Stream stream, string filePath)
+        {
+            using (FileStream fileStream = new FileStream(MapPath(filePath), FileMode.Append))
+            {
+                stream.CopyTo(fileStream);
+            }
+        }
+
+        public virtual void Delete(string filePath)
+        {
+            File.Delete(filePath);
+        }
+
+        public virtual string SaveFile(Stream stream, string fileName)
+        {
+            return SaveFile(stream, GenerateDictionary(fileName), fileName);
+        }
+
+        public virtual string SaveFile(Stream stream, string directory, string fileName)
+        {
+            string filePath = Path.Combine(MapPath(directory), fileName);
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                stream.CopyTo(fileStream);
+            }
+            return filePath;
+        }
+
+        public virtual void DeleteDirectory(string path)
+        {
+            Directory.Delete(path, true);
+        }
+    }
+}
