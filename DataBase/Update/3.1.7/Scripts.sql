38840f600289e0233417478cab38a60724be3d96
Wayne <seriawei@gmail.com>
2019-04-24 20:13:25 +0800

    Scripts update

---
+++ DataBase/Update/3.1.7/Scripts.sql
@@ -0,0 +1,4 @@
+﻿update Language set LanValue=N'英文名' where LanKey=N'ArticleEntity@Url' and CultureName=N'zh-CN'
+update Language set LanValue=N'英文名' where LanKey=N'ArticleType@Url' and CultureName=N'zh-CN'
+update Language set LanValue=N'英文名' where LanKey=N'ProductCategory@Url' and CultureName=N'zh-CN'
+update Language set LanValue=N'英文名' where LanKey=N'ProductEntity@Url' and CultureName=N'zh-CN'
