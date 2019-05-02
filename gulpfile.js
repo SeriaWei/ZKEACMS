8e3f494d74efcc7e13a6a464c6a09d720684bbcc
Wayne <seriawei@gmail.com>
2019-05-02 21:05:45 +0800

    gulp watch js and minify

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,24 @@ var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var minify = require('gulp-minify');
 
-function compileTheme(theme){
+
+function compileTheme(theme) {
     gulp.src(theme)
-    .pipe(sourcemaps.init())
-    .pipe(less())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(function (f) {
-        return f.base;
-    }))
-    .pipe(cleanCSS({inline: ['none']}))
-    .pipe(rename({
-        suffix: '.min'
-    }))
-    .pipe(gulp.dest(function (f) {
-        return f.base;
-    }));
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(function (f) {
+            return f.base;
+        }))
+        .pipe(cleanCSS({ inline: ['none'] }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(function (f) {
+            return f.base;
+        }));
 }
 
 gulp.task('Compile-Default-Theme', function (cb) {
@@ -26,7 +28,25 @@ gulp.task('Compile-Default-Theme', function (cb) {
     cb();
 });
 
-gulp.task('Watch-Theme', function (cb) {
+gulp.task('Auto-CompileTheme-MinifyJS', function (cb) {
     gulp.watch('src/ZKEACMS.WebHost/wwwroot/themes/Default/css/*.less', gulp.series('Compile-Default-Theme'));
+
+    var jsWatch = gulp.watch(['src/**/*.js', '!src/**/*.min.js']);
+    jsWatch.on('change', function (path, stats) {
+        console.log(`Minify ${path}`);
+        gulp.src(path)
+            .pipe(minify({
+                ext: { src: '.js', min: '.min.js' },
+                noSource: true,
+                preserveComments:"some"
+            }))
+            .on("error", function (error) {
+                console.log(error.toString())
+                this.emit('end')
+            })
+            .pipe(gulp.dest(function (f) {
+                return f.base;
+            }));
+    });
     cb();
 });
