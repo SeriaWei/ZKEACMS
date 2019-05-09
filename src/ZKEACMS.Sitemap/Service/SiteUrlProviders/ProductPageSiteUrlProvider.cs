6e8d7a06ba1a90cfd63d49c23c44a1256492653c
Wayne <seriawei@outlook.com>
2019-05-09 23:40:57 +0800

    use hashset

--- src/ZKEACMS.Sitemap/Service/SiteUrlProviders/ProductPageSiteUrlProvider.cs
+++ src/ZKEACMS.Sitemap/Service/SiteUrlProviders/ProductPageSiteUrlProvider.cs
@@ -22,7 +22,7 @@ namespace ZKEACMS.Sitemap.Service.SiteUrlProviders
         }
         public IEnumerable<SiteUrl> Get()
         {
-            List<string> excuted = new List<string>();
+            HashSet<string> excuted = new HashSet<string>();
             foreach (var item in _sitemapDbContext.ProductListWidget.ToList())
             {
                 if (item.DetailPageUrl.IsNotNullAndWhiteSpace() && !excuted.Contains(item.DetailPageUrl))
