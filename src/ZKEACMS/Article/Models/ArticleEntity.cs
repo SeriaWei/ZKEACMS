63775200637841f59f8931f244c2932531257ede
Wayne <seriawei@outlook.com>
2019-06-06 00:27:35 +0800

    ArticleEntity

--- src/ZKEACMS/Article/Models/ArticleEntity.cs
+++ src/ZKEACMS/Article/Models/ArticleEntity.cs
@@ -47,7 +47,7 @@ namespace ZKEACMS.Article.Models
                 .ShowInGrid();
 
             ViewConfig(m => m.ArticleContent).AsTextArea().AddClass(StringKeys.HtmlEditorClass);
-            ViewConfig(m => m.PublishDate).AsTextBox().FormatAsDateTime();
+            ViewConfig(m => m.PublishDate).AsTextBox().Hide();
             ViewConfig(m => m.IsPublish).AsTextBox().Hide().ShowInGrid();
         }
     }
