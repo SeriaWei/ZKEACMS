0a64f93d304409ec2c431c53e4384bd074f5f5ef
Wayne <seriawei@outlook.com>
2019-06-11 23:40:12 +0800

    Layout design advanced mode #301

--- src/ZKEACMS.WebHost/wwwroot/js/LayoutDesign.js
+++ src/ZKEACMS.WebHost/wwwroot/js/LayoutDesign.js
@@ -225,4 +225,89 @@
         form.submit();
         return false;
     });
+    $(document).on("click", "#show-source-code", function () {
+        var copyContainer = $('<div id="containers"/>').append(container.html());
+
+        $("div", copyContainer)
+            .removeClass("ui-droppable")
+            .removeClass("ui-sortable")
+            .removeClass("ui-sortable-handle")
+            .removeClass("active")
+            .removeClass("design")
+            .removeClass("custom-style-target")
+            .not(".custom-style").removeAttr("style");
+
+        $(".tools", copyContainer).remove();
+
+        var html = copyContainer.html()
+            .replace(new RegExp('><div', 'g'), ">\n<div")
+            .replace(new RegExp('></div', 'g'), ">\n</div")
+            .replace(new RegExp('><zone>', 'g'), ">\n<zone>\n")
+            .replace(new RegExp('></zone', 'g'), ">\n</zone")
+            .replace(new RegExp('><input', 'g'), ">\n<input");
+        editor.getDoc().setValue(html);
+        var totalLines = editor.lineCount();
+        editor.autoFormatRange({ line: 0, ch: 0 }, { line: totalLines });
+        setTimeout(function () {
+            editor.refresh();
+        }, 300);
+    });
+
+    CodeMirror.defineExtension("autoFormatRange", function (from, to) {
+        var cm = this;
+        var outer = cm.getMode(), text = cm.getRange(from, to).split("\n");
+        var state = CodeMirror.copyState(outer, cm.getTokenAt(from).state);
+        var tabSize = cm.getOption("tabSize");
+
+        var out = "", lines = 0, atSol = from.ch == 0;
+        function newline() {
+            out += "\n";
+            atSol = true;
+            ++lines;
+        }
+
+        for (var i = 0; i < text.length; ++i) {
+            var stream = new CodeMirror.StringStream(text[i], tabSize);
+            while (!stream.eol()) {
+                var inner = CodeMirror.innerMode(outer, state);
+                var style = outer.token(stream, state), cur = stream.current();
+                stream.start = stream.pos;
+                if (!atSol || /\S/.test(cur)) {
+                    out += cur;
+                    atSol = false;
+                }
+                if (!atSol && inner.mode.newlineAfterToken &&
+                    inner.mode.newlineAfterToken(style, cur, stream.string.slice(stream.pos) || text[i + 1] || "", inner.state))
+                    newline();
+            }
+            if (!stream.pos && outer.blankLine) outer.blankLine(state);
+            if (!atSol) newline();
+        }
+
+        cm.operation(function () {
+            cm.replaceRange(out, from, to);
+            for (var cur = from.line + 1, end = from.line + lines; cur <= end; ++cur)
+                cm.indentLine(cur, "smart");
+        });
+    });
+    CodeMirror.defineExtension("autoIndentRange", function (from, to) {
+        var cmInstance = this;
+        this.operation(function () {
+            for (var i = from.line; i <= to.line; i++) {
+                cmInstance.indentLine(i, "smart");
+            }
+        });
+    });
+    var editor = CodeMirror.fromTextArea(document.getElementById('layout-code'), {
+        lineNumbers: true,
+        mode: "htmlmixed",
+        theme: 'monokai',
+        lineWrapping: true,
+        autofocus: true,
+        tabSize: 2,
+        extraKeys: { "Ctrl-J": "autocomplete" },
+        autoCloseTags: true
+    });
+    $(".CodeMirror").height($(window).height() - 220);
+    $("#modal-layout-code").css("display", "");
 });
