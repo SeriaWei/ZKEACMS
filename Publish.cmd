28f71db390b0cdb96d57b5f5222d7447d1fd6bb8
Wayne <seriawei@outlook.com>
2019-06-14 20:16:17 +0800

    dotnet core preview6

--- Publish.cmd
+++ Publish.cmd
@@ -23,7 +23,7 @@ exit
 WHERE /Q publish-zkeacms
 IF %ERRORLEVEL% NEQ 0 (
 @echo Installing publish tool, please wait.
-dotnet tool install --global ZKEACMS.Publisher --version 1.5.5
+dotnet tool install --global ZKEACMS.Publisher --version 1.5.6-preview3.0
 )
 @pause
 @echo Publishing, please wait...
