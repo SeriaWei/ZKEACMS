1161c2b879173f4049a2ebfc670b1c9de73ff2e7
Wayne <seriawei@outlook.com>
2019-07-26 21:49:20 +0800

    Update packages

--- test/ZKEACMS.Test/SeleniumTester.cs
+++ test/ZKEACMS.Test/SeleniumTester.cs
@@ -1,40 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Text;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
 
-namespace ZKEACMS.Test
-{
-    public class SeleniumTester : IDisposable
-    {
+//namespace ZKEACMS.Test
+//{
+//    public class SeleniumTester : IDisposable
+//    {
 
-        public SeleniumTester()
-        {
-            ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService(Directory.GetCurrentDirectory());
-            chromeDriverService.HostName = "127.0.0.1";
+//        public SeleniumTester()
+//        {
+//            ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService(Directory.GetCurrentDirectory());
+//            chromeDriverService.HostName = "127.0.0.1";
 
-            chromeDriverService.HideCommandPromptWindow = true;
+//            chromeDriverService.HideCommandPromptWindow = true;
 
-            WebDriver = new ChromeDriver(chromeDriverService);
+//            WebDriver = new ChromeDriver(chromeDriverService);
 
-            WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-        }
+//            WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+//        }
 
-        public IWebDriver WebDriver { get; set; }
+//        public IWebDriver WebDriver { get; set; }
 
-        public virtual void GoToUrl(string url)
-        {
-            WebDriver.Navigate().GoToUrl($"http://localhost:5000{url}");
-        }
-        public void Wait(int seconds)
-        {
-            System.Threading.Thread.Sleep(seconds * 1000);
-        }
-        public void Dispose()
-        {
-            WebDriver.Quit();
-        }
-    }
-}
+//        public virtual void GoToUrl(string url)
+//        {
+//            WebDriver.Navigate().GoToUrl($"http://localhost:5000{url}");
+//        }
+//        public void Wait(int seconds)
+//        {
+//            System.Threading.Thread.Sleep(seconds * 1000);
+//        }
+//        public void Dispose()
+//        {
+//            WebDriver.Quit();
+//        }
+//    }
+//}
