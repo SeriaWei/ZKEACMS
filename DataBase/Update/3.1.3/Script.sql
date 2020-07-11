bdc12a5e590d166678d3db7b90676bff4cc0c8d1
wayne <seriawei@outlook.com>
2020-07-11 18:36:30 +0800

    Responsive image #372

--- DataBase/Update/3.1.3/Script.sql
+++
@@ -1,1 +0,0 @@
-﻿INSERT INTO [dbo].[Language] ([LanKey], [CultureName], [LanValue], [Module], [LanType]) VALUES (N'SiteSearchWidget@SearchEngine', N'zh-CN', N'搜索引擎', NULL, N'EntityProperty')
--- DataBase/Update/3.2.1/script.sql
+++
@@ -1,1 +0,0 @@
-INSERT [dbo].[Language] ([LanKey], [CultureName], [LanValue], [Module], [LanType]) VALUES (N'TabWidget@TabItems', N'zh-CN', N'标签项', N'TabWidget', N'EntityProperty')
