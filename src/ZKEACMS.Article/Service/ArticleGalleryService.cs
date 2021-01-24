abc6922a9307c1d08bd3fbc28d5e8148da5c3ee7
wayne <seriawei@outlook.com>
2021-01-24 11:44:39 +0800

    Update entry title when get gallery

--- src/ZKEACMS.Article/Service/ArticleGalleryService.cs
+++ src/ZKEACMS.Article/Service/ArticleGalleryService.cs
@@ -17,8 +17,28 @@ namespace ZKEACMS.Article.Service
 {
     public class ArticleGalleryService : ServiceBase<ArticleGallery>, IArticleGalleryService
     {
-        public ArticleGalleryService(IApplicationContext applicationContext, CMSDbContext dbContext) : base(applicationContext, dbContext)
+        private readonly IArticleService _articleService;
+        public ArticleGalleryService(IApplicationContext applicationContext, CMSDbContext dbContext, IArticleService articleService)
+            : base(applicationContext, dbContext)
         {
+            _articleService = articleService;
+        }
+        public override ArticleGallery Get(params object[] primaryKey)
+        {
+            var gallery = base.Get(primaryKey);
+            if (gallery != null)
+            {
+                var articleIds = gallery.Articles.Where(m => m.Article != null).Select(m => m.Article.ArticleID).ToArray();
+                if (articleIds.Length > 0)
+                {
+                    var articles = _articleService.Get(m => articleIds.Contains(m.ID));
+                    foreach (var item in gallery.Articles.Where(m => m.Article != null))
+                    {
+                        item.Article.Title = articles.FirstOrDefault(m => m.ID == item.Article.ArticleID)?.Title;
+                    }
+                }
+            }
+            return gallery;
         }
     }
 }
--- src/ZKEACMS.Product/Service/ProductGalleryService.cs
+++ src/ZKEACMS.Product/Service/ProductGalleryService.cs
@@ -17,8 +17,28 @@ namespace ZKEACMS.Product.Service
 {
     public class ProductGalleryService : ServiceBase<ProductGallery>, IProductGalleryService
     {
-        public ProductGalleryService(IApplicationContext applicationContext, CMSDbContext dbContext) : base(applicationContext, dbContext)
+        private readonly IProductService _productService;
+        public ProductGalleryService(IApplicationContext applicationContext, CMSDbContext dbContext, IProductService productService) : base(applicationContext, dbContext)
         {
+            _productService = productService;
+        }
+
+        public override ProductGallery Get(params object[] primaryKey)
+        {
+            var gallery = base.Get(primaryKey);
+            if (gallery != null)
+            {
+                var productIds = gallery.Products.Where(m => m.Product != null).Select(m => m.Product.ProductID).ToArray();
+                if (productIds.Length > 0)
+                {
+                    var products = _productService.Get(m => productIds.Contains(m.ID));
+                    foreach (var item in gallery.Products.Where(m => m.Product != null))
+                    {
+                        item.Product.Title = products.FirstOrDefault(m => m.ID == item.Product.ProductID)?.Title;
+                    }
+                }
+            }
+            return gallery;
         }
     }
 }
