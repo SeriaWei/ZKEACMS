d1e817eb870f64fe42948e058b23d4bc14ee58d6
wayne <seriawei@outlook.com>
2021-03-19 20:08:53 +0800

    script for 3.4.1

---
+++ Database/Update/3.4.1/MsSql.sql
@@ -0,0 +1,2 @@
+﻿DELETE FROM [Language] WHERE LanKey = N'File' AND CultureName = N'zh-CN';
+INSERT INTO [Language] (LanKey,CultureName,LanValue) values(N'File',N'zh-CN',N'文件');
---
+++ Database/Update/3.4.1/MySql.sql
@@ -0,0 +1,2 @@
+﻿DELETE FROM `Language` WHERE `LanKey` = 'File' AND `CultureName` = 'zh-CN';
+INSERT INTO `Language` (`LanKey`,`CultureName`,`LanValue`) values('File','zh-CN','文件');
