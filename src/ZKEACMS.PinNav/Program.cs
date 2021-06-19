e9504f6d812c6cf633febd622693e59b6e138856
wayne <seriawei@outlook.com>
2021-06-19 12:19:10 +0800

    PinNavPlug

---
+++ src/ZKEACMS.PinNav/Program.cs
@@ -0,0 +1,15 @@
+﻿/* http://www.zkea.net/ 
+ * Copyright (c) ZKEASOFT. All rights reserved. 
+ * http://www.zkea.net/licenses */
+
+
+namespace ZKEACMS.PinNav
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
