4b35f3f818e3906169f43f076d6bad10f7e1c56d
Wayne Wei <Wayne_Wei@Kingston.com.cn>
2022-10-19 10:53:13 +0800

    Fetch new antiforgory token.

--- src/ZKEACMS/Controllers/AntiforgeryController.cs
+++ src/ZKEACMS/Controllers/AntiforgeryController.cs
@@ -17,9 +17,31 @@ namespace ZKEACMS.Controllers
         }
         public IActionResult GetTokenSet()
         {
-            var tokenSet = _antiforgery.GetAndStoreTokens(HttpContext);
-            string content = $"/*! http://www.zkea.net/ Copyright (c) ZKEASOFT. All rights reserved. http://www.zkea.net/licenses */ var ZKEACMS = ZKEACMS || {{}}; ZKEACMS.AntiToken = {{ '{tokenSet.FormFieldName}': '{tokenSet.RequestToken}' }};";
-            return Content(content, "application/javascript");
+            try
+            {
+                var tokenSet = _antiforgery.GetAndStoreTokens(HttpContext);
+                string content = $"/*! http://www.zkea.net/ Copyright (c) ZKEASOFT. All rights reserved. http://www.zkea.net/licenses */ var ZKEACMS = ZKEACMS || {{}}; ZKEACMS.AntiToken = {{ '{tokenSet.FormFieldName}': '{tokenSet.RequestToken}' }};";
+                return Content(content, "application/javascript");
+            }
+            catch
+            {
+                string content = "/*! http://www.zkea.net/ Copyright (c) ZKEASOFT. All rights reserved. http://www.zkea.net/licenses */ var ZKEACMS = ZKEACMS || {}; ZKEACMS.AntiToken = {};fetch('"+ Url.Action("GetRequestToken") +"', { method: 'POST', cache: 'no-cache',headers: { 'Content-Type': 'application/json'} }).then(function (data) { return data.json() }).then(function (data) { ZKEACMS.AntiToken[data.key]=data.value; });";
+                return Content(content, "application/javascript");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult GetRequestToken()
+        {
+            try
+            {
+                var tokenSet = _antiforgery.GetAndStoreTokens(HttpContext);
+                return Json(new { key = tokenSet.FormFieldName, value = tokenSet.RequestToken });
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
     }
 }
