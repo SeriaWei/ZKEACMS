54a0d6ec569f5ea137c7bb57743bf66cbd159f36
wayne <seriawei@outlook.com>
2024-09-21 20:15:53 +0800

    Json clone.

---
+++ src/EasyFrameWork/Extend/ExtObject.cs
@@ -0,0 +1,21 @@
+﻿/* http://www.zkea.net/ 
+ * Copyright (c) ZKEASOFT. All rights reserved. 
+ * http://www.zkea.net/licenses */
+
+using Easy.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy.Extend
+{
+    public static class ExtObject
+    {
+        public static T Clone<T>(this T obj) where T : class
+        {
+            return JsonConverter.Deserialize<T>(JsonConverter.Serialize(obj));
+        }
+    }
+}
