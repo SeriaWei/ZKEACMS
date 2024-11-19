fcfa45778f947ffd690f55f2c06f0f3cdc82136a
wayne <seriawei@outlook.com>
2024-11-19 20:49:53 +0800

    Script 4.2

--- Database/Update/4.2/MsSql.sql
+++ Database/Update/4.2/MsSql.sql
@@ -1,8 +1,6 @@
 ALTER TABLE dbo.ArticleType ADD DisplayOrder int NULL
 GO
-
 ALTER TABLE dbo.ProductCategory ADD DisplayOrder int NULL
 GO
-
 ALTER TABLE dbo.VideoType ADD DisplayOrder int NULL
 GO
---
+++ Database/Update/4.2/Sqlite.sql
@@ -0,0 +1,3 @@
+ALTER TABLE ArticleType ADD DisplayOrder int NULL;
+ALTER TABLE ProductCategory ADD DisplayOrder int NULL;
+ALTER TABLE VideoType ADD DisplayOrder int NULL;
