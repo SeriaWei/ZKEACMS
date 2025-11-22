a52c342ec864f81c12d62c7c7c7075fe51d92e72
wayne <seriawei@outlook.com>
2025-11-22 23:06:18 +0800

    Should publish page successfully with widgets

--- test/End-To-End/tests/admin/page/01-basic-function-test.spec.ts
+++ test/End-To-End/tests/admin/page/01-basic-function-test.spec.ts
@@ -39,3 +39,44 @@ test('Should publish page successfully without any widgets', async ({ page }) =>
     await pageForm.publishPageInDesignView();
     await expect(page).toHaveURL(`/${formData.path}`);
 });
+
+test('Should publish page successfully with widgets', async ({ page }) => {
+    var pageForm = new PageFormPage(page);
+    await pageForm.login();
+    const formData = await createPage(pageForm);
+
+    await page.locator(".templates").evaluate((element) => {
+        if (!element.classList.contains("active")) {
+            element.classList.add("active");
+        }
+    });
+
+    const main = page.getByRole('link', { name: '+ 添加内容 | 主内容' });
+    
+    await page.getByRole('img', { name: '巨幕' }).dragTo(main);
+    await page.getByRole('img', { name: '页头' }).dragTo(main);
+    await page.getByRole('img', { name: '图片', exact: true }).dragTo(main);
+    await page.getByRole('img', { name: '段落' }).dragTo(main);
+    await page.getByRole('img', { name: '分隔符' }).dragTo(main);
+    await page.getByRole('img', { name: '间距' }).dragTo(main);
+    await page.getByRole('img', { name: '文字二列' }).dragTo(main);
+    await page.getByRole('img', { name: '文字三列' }).dragTo(main);
+    await page.getByRole('img', { name: '图片右', exact: true }).dragTo(main);
+    await page.getByRole('img', { name: '图片右（圆）' }).dragTo(main);
+    await page.getByRole('img', { name: '图片左', exact: true }).dragTo(main);
+    await page.getByRole('img', { name: '图片左（圆）' }).dragTo(main);
+    await page.getByRole('img', { name: '图片左（平分）' }).dragTo(main);
+    await page.getByRole('img', { name: '图例二', exact: true }).dragTo(main);
+    await page.getByRole('img', { name: '图例二（圆）' }).dragTo(main);
+    await page.getByRole('img', { name: '图例三', exact: true }).dragTo(main);
+    await page.getByRole('img', { name: '图例三（圆）' }).dragTo(main);
+
+    await page.locator(".templates").evaluate((element) => {
+        if (element.classList.contains("active")) {
+            element.classList.remove("active");
+        }
+    });
+
+    await pageForm.publishPageInDesignView();
+    await expect(page).toHaveURL(`/${formData.path}`);
+});
